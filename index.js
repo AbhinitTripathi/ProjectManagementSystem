b7c9298a69f71a075827ea7319797016e4479723
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-10-24 00:01:29 +0530

    Add env dupport

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Start of Backend Project");
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: "./.env",
+})
+
+const my_user_name = process.env.user_name;
+
+console.log(`Hello ${my_user_name}`);

44c887c59664bba08e628791dffe0e6d45a0470a
Abhinit Tripathi <114798873+AbhinitTripathi@users.noreply.github.com>
2025-11-19 23:16:03 +0530

    Add access token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // Define the schema structure for the 'User' collection
 const userSchema = new Schema(
@@ -90,6 +91,20 @@ userSchema.pre("save", async function (next) {
 // Schema Method to equate passwords byt hashing incoming & existing
 userSchema.methods.isPasswordCorrect = async function (password) {
   return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
 };
 
 // Exporting a Mongoose model called 'User' that uses the defined schema
